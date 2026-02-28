205ebb5372f956a96553b4eb46cbe5424635fa62
eunicecho33 <eunicecho33@gmail.com>
2026-02-28 16:18:32 -0600

    tweak

--- last.py
+++ last.py
@@ -1,11 +1,7 @@
 from dotenv import load_dotenv
 import os
-import hashlib
+import requests
 
 load_dotenv()
 LASTFM_KEY = os.getenv('LASTFM_KEY')
 LASTFM_SECRET = os.getenv('LASTFM_SECRET')
-
-LASTFM_SIGNATURE = hashlib.md5(
-    (LASTFM_KEY + "xxxxxxxx" + "method" + "auth.getSession" +
-     "token" + "xxxxxxx" + LASTFM_SECRET).encode('utf-8')).hexdigest()
