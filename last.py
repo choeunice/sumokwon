557c4396d283dc81676f8dbf24fbbdeafd28ae0f
eunicecho33 <eunicecho33@gmail.com>
2026-02-28 17:00:42 -0600

    last

--- last.py
+++ last.py
@@ -1,8 +1,19 @@
 from dotenv import load_dotenv
 import os
 import requests
+import json
 
 load_dotenv()
 LASTFM_KEY = os.getenv('LASTFM_KEY')
 LASTFM_SECRET = os.getenv('LASTFM_SECRET')
-print('hi')
+
+root = 'http://ws.audioscrobbler.com/2.0/'
+
+def get(payload):
+    '''
+    payload takes the form of key/value pair:
+    'method': '...'
+    '''
+    payload['api_key'] = LASTFM_KEY
+    payload['format'] = 'json'
+    return requests.get(root, params=payload)
