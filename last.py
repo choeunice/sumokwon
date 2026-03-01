714aca34887b95edb6fbe4a4c2130e18844abe79
eunicecho33 <eunicecho33@gmail.com>
2026-02-28 23:07:04 -0600

    last

--- last.py
+++ last.py
@@ -5,6 +5,7 @@ import os
 import requests
 import json
 import pandas as pd
+import re
 
 load_dotenv()
 LASTFM_KEY = os.getenv('LASTFM_KEY')
@@ -17,23 +18,42 @@ root = 'http://ws.audioscrobbler.com/2.0/'
 def get(payload):
     payload['api_key'] = LASTFM_KEY
     payload['format'] = 'json'
+    payload['autocorrect'] = 1
     return requests.get(root, params=payload)
 
 # GET top tags
 
-def get_top_tags(artist, track):
+def get_top_tags(tuple):
     
     def parse(j):
         data = j.json()
-        data = pd.DataFrame(data['toptags']['tag'])
-        return list(data[data['count'] >= 10]['name'])
+        if 'error' in data.keys():
+            raise Exception
+        else:
+            data = pd.DataFrame(data['toptags']['tag'])
+            if len(data) == 0:
+                return []
+            else:
+                return list(data[data['count'] >= 10]['name'])
+
+    try:
+        df = parse(get({'method': 'track.gettoptags',
+                        'artist': tuple[0],
+                        'track': tuple[1]}))
+        return df
+    except:
+        try:
+            correction = get({'method': 'track.getcorrection',
+                              'artist': tuple[0],
+                              'track': tuple[1]})
+            correction = correction.json()
+            if 'error' in correction.keys():
+                raise Exception
+        except:
+            return []
     
-    return parse(get({'method': 'track.gettoptags',
-                      'artist': artist,
-                      'track': track}))
-    
-test = get_top_tags('Joji', 'PIXELATED KISSES')
-print(test)
+# print(get_top_tags(('Joji', 'PIXELATED KISSES')))
+# print(get_top_tags(('PureSnow', "I'm Never Looking Back")))
 
 # GET tag info
 
@@ -46,14 +66,21 @@ def get_tag_info(tag):
     
     df = parse(get({'method': 'tag.getinfo',
                       'tag': tag})).loc['summary',]
-    wiki = df[['wiki']]
-    
-    def parse_2(j):
-        data = j.json()
-        data = pd.DataFrame(data)
-        print(data)
+    wiki = df[['wiki']].iloc[0]
+    wiki = re.split(' <a', wiki)[0]
+    wiki = re.sub('\n+', ' ', wiki)
+    return (tag, wiki)
 
-    parse_2(get({'method': 'tag.getsimilar',
-                 'tag': tag}))
+# print(get_tag_info('rage'))
+# print(get_tag_info('cloud rap'))
 
-test = get_tag_info('rock')
+# combine top tags and tag info
+
+def collect_tags(L):
+    tags = [get_top_tags(L[i]) for i in range(0, len(L))]
+    print(tags)
+
+L = [('Feng', 'Cali Crazy'), ('PureSnow', "I'm Never Looking Back"), ('Saam Sultan', 'Jump Shot')]
+print(collect_tags(L))
+
+print(get_top_tags(('Feng', 'Cali Crazy')))
